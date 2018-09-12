3be15da5d2597c129d205e38f67061c6d5256243
Alex Brown <abrown@armsbusinesssolutions.com>
2018-09-12 10:59:44 -0500

    add GET method to mapping builder

--- src/builders/index.js
+++ src/builders/index.js
@@ -5,6 +5,10 @@ module.exports = require('./matchers')
 
 Object.assign(module.exports, {
 
+  get: function (urlPattern) {
+    return new MappingBuilder('GET', urlPattern)
+  },
+
   post: function (urlPattern) {
     return new MappingBuilder('POST', urlPattern)
   },
