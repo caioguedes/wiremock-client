c633186ffeaf14dede533043538a5a9b87eec3a1
Alex Brown <abrown@armsbusinesssolutions.com>
2018-09-12 14:40:42 -0500

    add testing for http-admin-inteface

--- src/http-admin-interface.js
+++ src/http-admin-interface.js
@@ -15,6 +15,9 @@ module.exports = class HttpAdminInterface {
   }
 
   resetMappings () {
-    return rp.post(`${this.uri}/mappings/reset`)
+    return rp({
+      method: 'POST',
+      url: `${this.uri}/mappings/reset`
+    })
   }
 }
---
+++ src/http-admin-interface.spec.js
@@ -0,0 +1,53 @@
+const rewire = require('rewire')
+
+describe('HttpAdminInterface', () => {
+
+  let HttpAdminInterface
+  let rpSpy
+
+  beforeEach(() => {
+    rpSpy = jasmine.createSpy('rp')
+    HttpAdminInterface = rewire('./http-admin-interface')
+    HttpAdminInterface.__set__({
+      rp: rpSpy
+    })
+  })
+
+  describe('constructor', () => {
+
+    it('should build a uri from the hostname and port', () => {
+      const admin = new HttpAdminInterface('localhost', 8080)
+      expect(admin.uri).toEqual('http://localhost:8080/__admin')
+    })
+
+  })
+
+  describe('methods', () => {
+
+    let admin
+
+    beforeEach(() => {
+      admin = new HttpAdminInterface('localhost', 8080)
+    })
+
+    it('should have a method for creating mappings', () => {
+      const mapping = {test: 'mapping'}
+      admin.createMapping(mapping)
+      expect(rpSpy).toHaveBeenCalledWith({
+        method: 'POST',
+        url: `http://localhost:8080/__admin/mappings`,
+        body: mapping,
+        json: true
+      })
+    })
+
+    it('should have a method for resetting mappings', () => {
+      admin.resetMappings()
+      expect(rpSpy).toHaveBeenCalledWith({
+        method: 'POST',
+        url: `http://localhost:8080/__admin/mappings/reset`
+      })
+    })
+  })
+
+})
