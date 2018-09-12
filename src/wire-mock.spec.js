9a97c76557810259daa21a441de016c2b1edda06
Alex Brown <abrown@armsbusinesssolutions.com>
2018-09-11 23:36:40 -0500

    add jasmine support with single test

---
+++ src/wire-mock.spec.js
@@ -0,0 +1,14 @@
+const WireMock = require('./wire-mock')
+
+describe('WireMock', () => {
+
+  describe('constructor', () => {
+
+    it('should default host and port', () => {
+      const wireMock = new WireMock()
+      expect(wireMock.admin.uri).toEqual('http://localhost:8080/__admin')
+    })
+
+  })
+
+})
